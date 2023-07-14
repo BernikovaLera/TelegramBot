4a23f6b2a42f6f26b639c24105537a4711de0700
BernikovaLera <109979423+BernikovaLera@users.noreply.github.com>
2023-07-14 21:05:01 +0300

    Update main.py

--- WeatherBot/main.py
+++ WeatherBot/main.py
@@ -15,14 +15,16 @@ def start(massage):
 def get_weather(massage):
     city = massage.text.strip().lower()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-    data = json.loads(res.text)
-    temp = data["main"]["temp"]
-    bot.reply_to(massage, f'Сейчас погода: {temp}')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        bot.reply_to(massage, f'Сейчас погода: {temp}')
 
-    image = 'sunny.png' if temp > 5.0 else 'sun.png'
-    file = open('./' + image, 'rb')
-    bot.send_photo(massage.chat.id, file)
+        image = 'sunny.png' if temp > 5.0 else 'sun.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(massage.chat.id, file)
+    else:
+        bot.reply_to(massage, f'Город указан не верно')
 
 
 bot.polling(none_stop=True)
-
